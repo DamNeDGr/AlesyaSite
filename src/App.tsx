f906a1983414717725504854cfc16e16bd64afde
Damned <f89063431393@yandex.ru>
2026-02-25 23:08:40 +0400

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import { Header } from "./components/Header"
-import { Services } from "./components/Services"
 import useReveal from "./hooks/useReveal.ts"
-import { About } from "./components/About"
-import { ForWhom } from "./components/ForWhom"
-import { Result } from "./components/Result"
-import { Trust } from "./components/Trust"
-import { Reviews } from "./components/Reviews"
-import { Contacts } from "./components/Contacts"
 import { Footer } from "./components/Footer/Footer.tsx"
+import { HomePage } from "./Pages/HomePage/HomePage.tsx"
 
 function App() {
     useReveal()
     return (
         <>
             <Header />
-            <Services />
-            <About />
-            <ForWhom />
-            <Result />
-            <Trust />
-            <Reviews />
-            <Contacts />
+            <HomePage />
             <Footer />
         </>
     )
---
+++ src/Pages/HomePage/HomePage.tsx
@@ -0,0 +1,23 @@
+import useReveal from "../../hooks/useReveal.ts"
+import { Services } from "../../components/Services"
+import { About } from "../../components/About"
+import { ForWhom } from "../../components/ForWhom"
+import { Result } from "../../components/Result"
+import { Trust } from "../../components/Trust"
+import { Reviews } from "../../components/Reviews"
+import { Contacts } from "../../components/Contacts"
+
+export const HomePage = () => {
+    useReveal()
+    return (
+        <>
+            <Services />
+            <About />
+            <ForWhom />
+            <Result />
+            <Trust />
+            <Reviews />
+            <Contacts />
+        </>
+    )
+}
