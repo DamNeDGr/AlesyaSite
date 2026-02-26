658f77b34a2c40622e1440ad14cea0139f220ab9
DamNeDGr <f89063431393@yandex.ru>
2026-02-26 14:13:34 +0400

    install chadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import { Header } from "./components/Header"
-import useReveal from "./hooks/useReveal.ts"
-import { Footer } from "./components/Footer/Footer.tsx"
-import { HomePage } from "./Pages/HomePage/HomePage.tsx"
+import { HomePage } from "./Pages/HomePage/HomePage.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { MainLayout } from "./components/Layout/MainLayout";
+import { AdminLayout } from "./components/Layout/AdminLayout";
+import { AdminPage } from "./Pages/AdminPage";
+import { NotFoundPage } from "./Pages/ErrorPages/NotFoundPage";
 
 function App() {
-    useReveal()
     return (
         <>
-            <Header />
-            <HomePage />
-            <Footer />
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<MainLayout />}>
+                        <Route element={<HomePage />} index />
+                    </Route>
+                    <Route element={<AdminLayout />}>
+                        <Route path={"/admin"} element={<AdminPage />} />
+                    </Route>
+                    <Route>
+                        <Route path={"*"} element={<NotFoundPage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
