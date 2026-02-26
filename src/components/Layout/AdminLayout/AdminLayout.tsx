24a1eaa0426a3d026847e0969682ed7ddf95fbda
DamNeDGr <f89063431393@yandex.ru>
2026-02-26 14:12:33 +0400

    Add admin layout and Admin Page

---
+++ src/components/Layout/AdminLayout/AdminLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router";
+
+export const AdminLayout = () => {
+    return (
+        <>
+            <h2>Admin Layout</h2>
+            <Outlet />
+        </>
+    );
+};
