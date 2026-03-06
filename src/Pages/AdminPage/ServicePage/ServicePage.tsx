76c976ff6fa20bf33d9dcd97b08a72a3f4cfac0a
DamNeDGr <f89063431393@yandex.ru>
2026-03-06 14:05:42 +0400

    Вывел всю логику и работу сервисов с апи в хук

--- src/Pages/AdminPage/ServicePage/ServicePage.tsx
+++ src/Pages/AdminPage/ServicePage/ServicePage.tsx
@@ -1,80 +1,27 @@
 import { ServiceTable } from "@/components/AdminPageComponents/DataTable/ServiceTable";
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
+import { useEffect } from "react";
 import { ProfileMenu } from "@/components/AdminPageComponents/ProfileMenu";
 import { ModalBlur } from "@/components/AdminPageComponents/ModalBlur";
 import { FormAddService } from "@/components/AdminPageComponents/FormAddService/FormAddService.tsx";
 import { Link } from "react-router";
 import { Breadcrumb } from "antd";
-import { createService, deleteService, getServices, updateService } from "@/api/Services.api.ts";
-import type { IService } from "@/types/services.type.ts";
+import { useService } from "@/hooks/useService.ts";
 
 export const ServicePage = () => {
-    const [services, setServices] = useState<IService[] | null>(null);
-    const [editingService, setEditingService] = useState<IService | null>(null);
-    const [loading, setLoading] = useState(false);
-    const [isOpenModalService, setIsOpenModalService] = useState(false);
-    const [isOpenModalEditService, setIsOpenModalEditService] = useState(false);
-
-    const handleGetService = async () => {
-        setLoading(true);
-        try {
-            const res = await getServices();
-            setServices(res);
-        } catch (error) {
-            if (error instanceof Error) {
-                toast.error(error.message);
-            }
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const handleEditService = async (service: Omit<IService, "id">, serviceId: number) => {
-        setLoading(true);
-        try {
-            await updateService(service, serviceId);
-            toast.success("Данные об услугах успешно изменены");
-            await handleGetService();
-            setIsOpenModalEditService(false);
-        } catch (error) {
-            if (error instanceof Error) {
-                toast.error(error.message);
-            }
-        } finally {
-            setLoading(false);
-        }
-    };
-    const handleDeleteService = async (id: number) => {
-        setLoading(true);
-        try {
-            await deleteService(id);
-            toast.success("Услуга удалена");
-            await handleGetService();
-        } catch (error) {
-            if (error instanceof Error) {
-                toast.error(error.message);
-            }
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const handleCreateService = async (data: Omit<IService, "id">) => {
-        setLoading(true);
-        try {
-            await createService(data);
-            toast.success("Услуга создана");
-            setIsOpenModalService(false);
-            await handleGetService();
-        } catch (error) {
-            if (error instanceof Error) {
-                toast.error(error.message);
-            }
-        } finally {
-            setLoading(false);
-        }
-    };
+    const {
+        loading,
+        services,
+        currentService,
+        setCurrentService,
+        openModalService,
+        setOpenModalService,
+        openModalUpdateService,
+        setOpenModalUpdateService,
+        handleGetService,
+        handleEditService,
+        handleDeleteService,
+        handleCreateService,
+    } = useService();
 
     useEffect(() => {
         void handleGetService();
@@ -82,7 +29,7 @@ export const ServicePage = () => {
 
     return (
         <>
-            <ProfileMenu title={"Список услуг"} btnTitle={"Добавить услугу"} openAppoint={() => setIsOpenModalService(true)} />
+            <ProfileMenu title={"Список услуг"} btnTitle={"Добавить услугу"} openAppoint={() => setOpenModalService(true)} />
             <Breadcrumb
                 style={{
                     padding: "5px 0",
@@ -106,18 +53,18 @@ export const ServicePage = () => {
             {services !== null && (
                 <ServiceTable
                     services={services}
-                    onEdit={setEditingService}
-                    openEdit={() => setIsOpenModalEditService(true)}
+                    onEdit={setCurrentService}
+                    openEdit={() => setOpenModalUpdateService(true)}
                     onDelete={handleDeleteService}
                     loading={loading}
                 />
             )}
-            <ModalBlur open={isOpenModalService} onClose={() => setIsOpenModalService(false)}>
+            <ModalBlur open={openModalService} onClose={() => setOpenModalService(false)}>
                 <FormAddService onCreate={handleCreateService} />
             </ModalBlur>
 
-            <ModalBlur open={isOpenModalEditService} onClose={() => setIsOpenModalEditService(false)}>
-                {editingService !== null && <FormAddService onEdit={handleEditService} service={editingService} />}
+            <ModalBlur open={openModalUpdateService} onClose={() => setOpenModalUpdateService(false)}>
+                {currentService !== null && <FormAddService onEdit={handleEditService} service={currentService} />}
             </ModalBlur>
         </>
     );
---
+++ src/hooks/useService.ts
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import type { IService } from "@/types/services.type.ts";
+import { createService, deleteService, getServices, updateService } from "@/api/Services.api.ts";
+import { toast } from "react-toastify";
+import { handleError } from "@/helpers/errorHelper.ts";
+
+export const useService = () => {
+    const [services, setServices] = useState<IService[]>([]);
+    const [currentService, setCurrentService] = useState<IService | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [openModalService, setOpenModalService] = useState(false);
+    const [openModalUpdateService, setOpenModalUpdateService] = useState(false);
+
+    const handleGetService = async () => {
+        setLoading(true);
+        try {
+            const res = await getServices();
+            setServices(res);
+        } catch (error) {
+            handleError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleEditService = async (service: Omit<IService, "id">, serviceId: number) => {
+        setLoading(true);
+        try {
+            await updateService(service, serviceId);
+            toast.success("Данные об услугах успешно изменены");
+            await handleGetService();
+            setOpenModalUpdateService(false);
+        } catch (error) {
+            handleError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleDeleteService = async (id: number) => {
+        setLoading(true);
+        try {
+            await deleteService(id);
+            toast.success("Услуга удалена");
+            await handleGetService();
+        } catch (error) {
+            handleError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleCreateService = async (data: Omit<IService, "id">) => {
+        setLoading(true);
+        try {
+            await createService(data);
+            toast.success("Услуга создана");
+            setOpenModalService(false);
+            await handleGetService();
+        } catch (error) {
+            handleError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return {
+        loading,
+        services,
+        currentService,
+        setCurrentService,
+        openModalService,
+        setOpenModalService,
+        openModalUpdateService,
+        setOpenModalUpdateService,
+        handleGetService,
+        handleEditService,
+        handleDeleteService,
+        handleCreateService,
+    };
+};
